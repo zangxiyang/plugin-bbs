47393dab1edcfc654b14e001fca3c65398284a6f
Seale <zangxiyang@weidian.com>
2022-12-03 16:15:21 +0800

    change setting

--- src/main/java/run/halo/bbs/StarterPlugin.java
+++ src/main/java/run/halo/bbs/StarterPlugin.java
@@ -1,16 +1,16 @@
-package run.halo.starter;
+package run.halo.bbs;
 
+import lombok.extern.slf4j.Slf4j;
 import org.pf4j.PluginWrapper;
 import org.springframework.stereotype.Component;
-import run.halo.app.extension.Scheme;
-import run.halo.app.extension.SchemeManager;
 import run.halo.app.plugin.BasePlugin;
 
 /**
- * @author guqing
+ * @author Seale
  * @since 2.0.0
  */
 @Component
+@Slf4j
 public class StarterPlugin extends BasePlugin {
 
     public StarterPlugin(PluginWrapper wrapper) {
@@ -19,7 +19,7 @@ public class StarterPlugin extends BasePlugin {
 
     @Override
     public void start() {
-        System.out.println("插件启动成功！");
+        log.info("插件启动成功");
     }
 
     @Override
